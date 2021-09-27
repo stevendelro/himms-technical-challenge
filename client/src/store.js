83542e1f87236a74425190a813e9b46261053ba8
Steven Del Rosario <stevendelro@pm.me>
2021-09-27 18:20:23 -0500

    added spamReducer to redux store

--- client/src/store.js
+++ client/src/store.js
@@ -1,13 +1,13 @@
-import { combineReducers, applyMiddleware, compose, createStore } from 'redux';
+import { combineReducers, applyMiddleware, compose, createStore } from 'redux'
 import thunk from 'redux-thunk'
+import spamReducer from './reducers/spamReducer'
 
 //ADD REDUCERS HERE
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  spam: spamReducer,
+})
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
-export default store;
+export default store
