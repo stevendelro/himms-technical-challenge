0e7593a47d9c94d4760f751249eef810de21cf4e
Steven Del Rosario <stevendelro@pm.me>
2021-09-27 18:17:55 -0500

    handled getReports, blockReport, and resolveReport routes in /server

---
+++ server/routes/reportsRoute.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import * as reportsController from '../controllers/reportsController.js'
+
+const router = express.Router()
+
+router.get('/', reportsController.getReports, (req, res) => {
+  res.status(200).json({ reports: res.locals.reports })
+})
+
+router.put('/:reportId', reportsController.resolveReport, (req, res) => {
+  res.status(202).json({ resolvedReport: res.locals.updatedReport})
+})
+
+router.post('/block/:reportId', reportsController.blockReport, (req, res) => {
+  res.status(202).json({ blockedReport: res.locals.blockedReport })
+})
+
+export default router
