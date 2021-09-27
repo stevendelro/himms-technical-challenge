3391245763353a7f737feaf040acdb527c168c8a
Steven Del Rosario <stevendelro@pm.me>
2021-09-27 17:24:44 -0500

    updated homepage to render ReportList

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,12 +1,16 @@
+import LinearProgress from '@material-ui/core/LinearProgress';
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-import LinearProgress from '@material-ui/core/LinearProgress';
+
+import ReportsList from '../../components/ReportsList';
 
 export default function Home({ loading }) {
   return (
     <>
       {loading && <LinearProgress color="secondary" />}
-      <Typography variant="h3">home page</Typography>
+      <Typography variant="h3">
+        <ReportsList />
+      </Typography>
     </>
   );
 }
