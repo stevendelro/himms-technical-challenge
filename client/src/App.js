a2749fd257f5c2534bf67421d52326188013adef
Steven Del Rosario <stevendelro@pm.me>
2021-09-27 17:18:45 -0500

    connected App.js to redux store + setup top-level useEffect to retrieve data onMount

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import { bindActionCreators } from 'redux'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { connect } from 'react-redux'
+import React, { useEffect } from 'react'
+
+import { fetchReports } from './actions/actionCreators'
 import Home from '../src/pages/Home'
 
-export default function App() {
+const App = ({ fetchReports, loading }) => {
+  useEffect(() => fetchReports(), [])
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route exact path='/'>
+          <Home loading={loading} />
+        </Route>
       </Switch>
     </Router>
-  );
+  )
 }
+
+const mapStateToProps = store => ({ loading: store.spam.isLoading })
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ fetchReports }, dispatch)
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
