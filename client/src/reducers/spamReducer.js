84f12ca686581d9dd2490030b29e41dbb4553f88
Steven Del Rosario <stevendelro@pm.me>
2021-09-27 17:13:57 -0500

    spamReducer now handles cases for all actionTypes

---
+++ client/src/reducers/spamReducer.js
@@ -0,0 +1,61 @@
+import * as actions from '../actions/actions'
+
+const initialState = {
+  reports: [],
+  isLoading: false,
+  error: {},
+}
+
+const spamReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case actions.FETCH_REPORTS_STARTED:
+      return {
+        ...state,
+        isLoading: true,
+      }
+    case actions.FETCH_REPORTS_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+      }
+    case actions.FETCH_REPORTS_ERROR:
+    case actions.UPDATE_REPORT_ERROR:
+      return {
+        ...state,
+        error: { ...error },
+      }
+    case actions.FETCH_REPORTS:
+      return {
+        ...state,
+        reports: [...payload.reports],
+      }
+    case actions.BLOCK_REPORT:
+      const withBlockedUpdate = []
+      state.reports.forEach(report => {
+        if (report.id === payload.blockedReport.id) {
+          report.state = payload.blockedReport.state
+        }
+        withBlockedUpdate.push(report)
+      })
+      return {
+        ...state,
+        reports: withBlockedUpdate
+      }
+    case actions.RESOLVE_REPORT:
+      const withResolvedUpdate = []
+      state.reports.forEach(report => {
+        if (report.id === payload.resolvedReport.id) {
+          report.state = payload.resolvedReport.state
+        }
+        withResolvedUpdate.push(report)
+      })
+      return {
+        ...state,
+        reports: withResolvedUpdate
+      }
+    default:
+      return state
+  }
+}
+
+export default spamReducer
